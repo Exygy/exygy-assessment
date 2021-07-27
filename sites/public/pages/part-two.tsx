aba43fcab0954583f39c4baa0e5f4f46349defaf
Emily Jablonski <emily.jablonski@exygy.com>
2021-07-27 15:31:41 -0600

    remove part two scaffolding

--- sites/public/pages/part-two.tsx
+++ sites/public/pages/part-two.tsx
@@ -1,9 +1,8 @@
-import React, { useContext } from "react"
+import React from "react"
 import axios from "axios"
 import Head from "next/head"
-import { AuthContext, PageHeader, Form, FormCard, Button } from "@bloom-housing/ui-components"
+import { PageHeader, FormCard } from "@bloom-housing/ui-components"
 import { Listing } from "@bloom-housing/backend-core/types"
-import { useForm } from "react-hook-form"
 import Layout from "../layouts/application"
 
 export interface PartTwoProps {
@@ -11,15 +10,6 @@ export interface PartTwoProps {
 }
 
 export default function PartTwo(props: PartTwoProps) {
-  const { listingsService } = useContext(AuthContext)
-  const { handleSubmit } = useForm()
-
-  const onSubmit = async () => {
-    // await listingsService.update({
-    //   // Updated data
-    // })
-  }
-
   return (
     <Layout>
       <Head>
@@ -29,14 +19,7 @@ export default function PartTwo(props: PartTwoProps) {
       <section className="bg-gray-300">
         <div className="md:mb-20 md:mt-12 mx-auto max-w-lg print:my-0 print:max-w-full">
           <FormCard>
-            <div className="form-card__group">
-              <Form id="sign-in" onSubmit={handleSubmit(onSubmit)}>
-                Your form goes here
-                <div className="text-center mt-6">
-                  <Button>Submit</Button>
-                </div>
-              </Form>
-            </div>
+            <div className="form-card__group">Your form goes here</div>
           </FormCard>
         </div>
       </section>
